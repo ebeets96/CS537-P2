e1390a58b97d4bb8198d3d4882fd193e5fe554d8
Jack David <jackd@snares-04.cs.wisc.edu>
2018-10-15 22:04:46 -0500

    Implemented munch2 and reader

--- munch2.c
+++ munch2.c
@@ -1,1 +1,14 @@
 #include "munch2.h"
+#include <ctype.h>
+#include "queue.h"
+
+void munch2(Queue* q, char* str) {
+	char* ptr = str;
+	while (*ptr != NULL) {
+		if (*ptr.isLower()) {
+			*ptr = toupper((unsigned char) *ptr);
+		}
+		ptr++;
+	}
+	EnqueueString(q, str);
+}
--- reader.c
+++ reader.c
@@ -1,1 +1,24 @@
 #include "reader.h"
+#include <stdlib.h>
+
+void read(Queue* q) {
+	char* str = calloc(100, sizeof(char));
+	while (fgets(str, 100, stdin)) {
+		if (strchr(str, NULL) == NULL) {
+			fprintf(stderr, "Input line length longer than buffer length\n");
+			// Flush to end of line
+			int endOfLineFound = 0;
+			while (!endOfLineFound) {
+				fgets(str, 100, stdin);
+				if (strchr(str, NULL) != NULL) {
+					endOfLineFound = 1;
+				}
+			}
+		}
+		else {
+			EnqueueString(q, str);
+			str = calloc(100, sizeof(char));
+		}
+	}
+	free(str);
+}
