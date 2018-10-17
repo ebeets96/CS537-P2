6e05979880737dcbfbc0f64351bd0ce370c5487c
Eric Beets <ebeets@wisc.edu>
2018-10-17 11:38:38 -0500

    munch1 implementation

--- munch1.c
+++ munch1.c
@@ -1,1 +1,20 @@
+#include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
 #include "munch1.h"
+#include "queue.h"
+
+void munch1(Queue* input, Queue* output) {
+	char* string = DequeueString(input);
+	while(string != NULL) {
+		char* nextOccurence = index(string, ' ');
+		while(nextOccurence != NULL)
+			*nextOccurence = '*';
+			nextOccurence = index(string, ' ');
+		}
+
+
+		string = DequeueString(input);
+	}
+	EnqueueString(output, NULL);
+}
